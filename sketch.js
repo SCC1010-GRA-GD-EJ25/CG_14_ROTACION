d8e8c703b39f14ef0009c0a8b6c64c8070923c8c
CarlosGarciaOb <127284600+CMgarciaOB@users.noreply.github.com>
2025-03-04 22:52:21 +0000

    epilepsia

--- sketch.js
+++ sketch.js
@@ -1,14 +1,35 @@
-function preload() {
-  // put preload code here
-}
+let grado = Math.PI/180
+let r=grado
 
 function setup() {
   // put setup code here
   createCanvas(800,800)
-}
+rectMode(CENTER)
+background(255)
 
+}
+function dibujo(x,y,grados,){
+  push()
+  translate(x,y)
+  rotate(grados)
+  rect(0,0,100,100)
+  pop()
+
+
+}
 function draw() {
-  // put drawing code here
-  background(255)
-  rect(100,100,200,200)
+
+  text("🐻",mouseX,mouseY)
+  textSize(50)
+for(let i=0;i<20;i++){
+  let x= random(0,width)
+  let y= random(0,height)
+  let rojo= random(0,255)
+  let verde= random(0,255)
+  let azul= random(0,255)
+  fill(rojo,verde,azul)
+  dibujo(x,y,r)
+
+  r+=grado
+}
 }
